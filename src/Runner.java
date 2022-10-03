bab7221f3957612ca2a1531ae332f372416e84ac
liquancool <liquanl3@nycstudents.net>
2022-10-03 12:34:15 -0400

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,11 @@
+public class Runner {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = r1;
+        r1 = null;
+        r1.printRectangleInfo();
+
+
+
+    }
+}
